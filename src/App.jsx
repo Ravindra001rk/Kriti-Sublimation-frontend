08a4f5c6d094e4415cf923c63cfef3ff464f120c
Ravindra Kushwaha <ravindrakushwaha2k20@gmail.com>
2026-02-10 18:35:34 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Contact from "./Pages/Contact";
+import Products from "./Pages/Products";
+import Gallery from "./Pages/Gallery";
+import About from "./Pages/About";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
